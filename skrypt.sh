10543a37d49447503d90b5de2781635fd4aed815
Aleksy Greiveldinger <s32987@pjwstk.edu.pl>
2025-01-15 16:15:31 -0500

    Skrypt tworzy strukture plikow.

---
+++ skrypt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir {Dokumenty,Praca\ inzynierska,Zadania\ UKOS}
+mkdir Dokumenty/{Muzyka,Pliki\ tekstowe,Zaliczenie}
+mkdir Zadania\ UKOS/{Cwiczenia\ 1,Cwiczenia\ 2,Kolo}
+mkdir Zadania\ UKOS/Kolo/{Rozwiazanie,Tymczasowe}
